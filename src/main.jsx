d7ac8f527229f59a64246f56e04ab7fab54ea011
AbdulMoeez21 <moizrao7@gmail.com>
2023-03-21 23:52:16 +0500

    boiler template

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import 'antd-css-utilities/utility.min.css'
+ReactDOM.render(<App />, document.getElementById("root"));
